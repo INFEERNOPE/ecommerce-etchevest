f3dd6b623c86da34f07b81cce8d9497076ea8d91
Iñaki Etchevest <i.etchevesttt@gmail.com>
2022-08-30 09:57:57 -0300

    first commit

---
+++ sugarSyntax.js
@@ -0,0 +1,13 @@
+let number1 = 40;
+
+/*if (number1 < 30) {
+    console.log ("El numero es menor a 30")
+} else {
+    console.log ("El número es mayor o igual a 30");
+}*/
+
+//Sin SugarSyntax
+
+const stringDevuelto = number1 < 30 ? "El numero es menor a 30"  : "El numero es mayor o igual a 30";
+
+console.log (stringDevuelto);
