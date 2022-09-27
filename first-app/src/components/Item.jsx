7f99b9094d65e884aa9b55d08c01591687f4caf4
Iñaki Etchevest <i.etchevesttt@gmail.com>
2022-09-27 19:37:45 -0300

    first-app

--- first-app/src/components/Item.jsx
+++ first-app/src/components/Item.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 
-const Item = () => {
-  return (
-    <div className="card" style="width: 18rem;">
-  <img src="..." className="card-img-top" alt="..."/>
-  <div className="card-texto">
-    <p className="card-texto">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
+const Item = () => {  return (  
+
+  <div className="card" style={{width: '18rem'}}> 
+
+ <img src="..." className="card-img-top" alt="..."/> 
+
+ <div className="card-texto">   
+
+ <p className="card-texto">Some quick example text to build on the card title and make up the bulk of the card's content.</p> 
+
+ </div>
+
 </div>
   )
 }
